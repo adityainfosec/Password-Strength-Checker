b4a7fff13025ff735cf2d5433250dee1e2b88597
Aditya Gupta <aditya1560mm@gmail.com>
2026-06-22 19:13:36 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,25 +1,56 @@
 password = input("Enter your password: ")
 
 score = 0
+suggestions = []
 
+# Length check
 if len(password) >= 8:
-    score += 1
+    score += 20
+else:
+    suggestions.append("Increase password length to at least 8 characters")
 
+# Uppercase check
 if any(char.isupper() for char in password):
-    score += 1
+    score += 20
+else:
+    suggestions.append("Add uppercase letters")
 
+# Lowercase check
 if any(char.islower() for char in password):
-    score += 1
+    score += 20
+else:
+    suggestions.append("Add lowercase letters")
 
+# Digit check
 if any(char.isdigit() for char in password):
-    score += 1
+    score += 20
+else:
+    suggestions.append("Add numbers")
 
+# Special character check
 if any(not char.isalnum() for char in password):
-    score += 1
+    score += 20
+else:
+    suggestions.append("Add special characters")
 
-if score == 5:
+# Common weak passwords
+common_passwords = ["123456", "password", "admin", "qwerty"]
+if password.lower() in common_passwords:
+    score = 10
+    suggestions.append("Avoid common weak passwords")
+
+# Output
+print("\nPassword Strength Score:", score, "/100")
+
+if score >= 80:
     print("Strong Password")
-elif score >= 3:
+elif score >= 50:
     print("Medium Password")
 else:
     print("Weak Password")
+
+# Suggestions
+if suggestions:
+    print("\nSuggestions:")
+    for s in suggestions:
+        print("-", s)
